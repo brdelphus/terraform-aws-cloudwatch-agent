dd9471d0b31341a5bb000500401686b5294d7ef9
brdelphus <delphus@delphus.org>
2022-05-27 18:49:00 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "label" {
-  source     = "git::https://github.com/brdelphus/terraform-null-label.git?ref=tags/master"
+  source     = "git::https://github.com/brdelphus/terraform-null-label.git?ref=master"
   stage      = "${var.stage}"
   name       = "${var.name}"
   namespace  = "${var.namespace}"
